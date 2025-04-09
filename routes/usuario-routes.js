b826cee54c5b905b8f1225060db509cecfe75cd1
RodXorDevX <rodxordevx@gmail.com>
2025-04-09 02:56:46 -0400

    update

--- routes/usuario-routes.js
+++ routes/usuario-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const usuarioController = require('../controllers/usuario-controller');
 
 // Ruta GET /usuarios para obtener todos los usuarios
